a8d2d851b83c0c4916d84de4844f0144b8450e52
santiagoAR-ai <santiagoabdala270@gmail.com>
2025-03-28 12:50:39 -0300

    Final

--- src/test/java/TestEjercicio1.java
+++ src/test/java/TestEjercicio1.java
@@ -13,7 +13,9 @@ public class TestEjercicio1 {
         LocalDate fechaInicio = LocalDate.of(2025, 3, 1);
         LocalDate fechaCierre = LocalDate.of(2025, 3, 5);
         LocalDate fechaActual = LocalDate.of(2025, 3, 1);
-        Concurso concurso = new Concurso(fechaInicio, fechaCierre, fechaActual);
+        Concurso concurso = new Concurso(LocalDate.now(),
+                LocalDate.now().plusDays(7),
+                LocalDate.now());
         concurso.inscribirParticipante(participante);
         assertTrue(concurso.estaIncripto(participante));
         assertEquals(10, participante.getPuntos());
@@ -23,10 +25,9 @@ public class TestEjercicio1 {
     public void test02() {
         Participante participante1 = new Participante("Sofia", "Perez");
         Participante participante2 = new Participante("Juan", "Pérez");
-        LocalDate fechaInicio = LocalDate.of(2025, 3, 1);
-        LocalDate fechaCierre = LocalDate.of(2025, 3, 5);
-        LocalDate fechaActual = LocalDate.of(2025, 3, 2);
-        Concurso concurso = new Concurso(fechaInicio, fechaCierre, fechaActual);
+        Concurso concurso = new Concurso(LocalDate.now().minusDays(2),
+                LocalDate.now().plusDays(7),
+                LocalDate.now().plusDays(2));
         concurso.inscribirParticipante(participante1);
         concurso.inscribirParticipante(participante2);
         assertEquals(2, concurso.cantParticipante());
@@ -37,11 +38,9 @@ public class TestEjercicio1 {
     @Test
     public void test03() {
         Participante participante = new Participante("Carlos", "Gomez");
-        LocalDate fechaInicio = LocalDate.of(2025, 3, 1);
-        LocalDate fechaCierre = LocalDate.of(2025, 3, 5);
-        LocalDate fechaActual = LocalDate.of(2025, 3, 7);
-        Concurso concurso = new Concurso(fechaInicio, fechaCierre, fechaActual);
-
+        Concurso concurso = new Concurso(LocalDate.now().minusDays(1),
+                LocalDate.now().plusDays(5),
+                LocalDate.now().plusDays(8));
         assertThrows(RuntimeException.class, () -> {
             concurso.inscribirParticipante(participante);
         });
